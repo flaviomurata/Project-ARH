1d3601cf5483af6d86a8cbf1cd7d3ff9c12a3fb5
VictorGasperi <victorgasperi@gmail.com>
2023-05-17 19:31:39 -0300

    Commit_Teste

--- arh/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ arh/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [FormerlySerializedAs("rb")] [SerializeField]
+    private Rigidbody2D _rb;
+
+    [FormerlySerializedAs("groundCheck")] [SerializeField]
+    private Transform _groundCheck;
+
+    [FormerlySerializedAs("groundLayer")] [SerializeField]
+    private LayerMask _groundLayer;
+
+    private int _counter = 0;
+    private float _horizontal;
+    private float _speed = 2f;
+    private float _jumpPower = 8f;
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _rb.velocity = new Vector2(_horizontal * _speed, _rb.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (IsGrounded() || _counter < 2)
+            {
+                _counter++;
+                _rb.velocity = new Vector2(_rb.velocity.x, _jumpPower);
+            }
+        }
+
+        if (context.canceled && _rb.velocity.y > 0f)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        _counter = 0;
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
     }
 }
