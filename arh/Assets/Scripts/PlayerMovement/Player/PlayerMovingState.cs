181974e58d331b2072bf61709ad447e1dbfe03b4
edu510 <www.eduardo.f.n@gmail.com>
2023-05-17 21:08:42 -0300

    Movimento horizontal
    O player para instantaneamente

---
+++ arh/Assets/Scripts/PlayerMovement/Player/PlayerMovingState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovingState : PlayerBaseState
+{
+    public override void enterState(PlayerStateManager player)
+    {
+        //setar animacao
+        player.rb.velocity = new Vector2(10 * player.movement.action.ReadValue<float>(), player.rb.velocity.y);
+        Debug.Log("aaa");
+    }
+
+    public override void updateState(PlayerStateManager player)
+    {
+        
+    }
+}
---
+++ arh/Assets/Scripts/PlayerMovement/Player/PlayerStateManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerStateManager : MonoBehaviour
+{
+    PlayerBaseState currentState;
+    PlayerIdleState idleState = new PlayerIdleState();
+    PlayerMovingState movingState = new PlayerMovingState();
+
+    [SerializeField] public InputActionReference movement, jump;
+
+    public Rigidbody2D rb;
+
+    private void OnEnable()
+    {
+        movement.action.performed += move;
+        movement.action.canceled += stopMoving;
+    }
+
+    private void OnDisable()
+    {
+        movement.action.performed -= move;
+        movement.action.canceled -= stopMoving;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = idleState;
+        currentState.enterState(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void move(InputAction.CallbackContext obj)
+    {
+        currentState = movingState;
+        currentState.enterState(this);
+    }
+
+    private void stopMoving(InputAction.CallbackContext obj)
+    {
+        currentState = idleState;
+        currentState.enterState(this);
+    }
+}
